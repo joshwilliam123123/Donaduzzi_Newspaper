9adbf01f1d5617cdf10d3a9377a767fc87cbc9cf
Murilo Lang Alves <murilo.lang.alves@gmail.com>
2026-06-02 18:10:38 +0000

    News papers html

--- src/Pages/newsFeed/newsFeed.jsx
+++ src/Pages/newsFeed/newsFeed.jsx
@@ -2,9 +2,72 @@ import styles from './newsFeed.module.css'
 
 const NewsFeed = () => {
     return (
-        <article>
+        <main className={styles.main}> {/*Main*/}
 
-        </article>
+            <section className={styles.sidebar}>  {/*Sidebar*/}
+
+                <img className={styles.Logosuperior} src={Logocolegio} alt="" /> {/*Sidebar logo*/}
+                <div className={styles.buttonssidebar}> {/*Side bar*/}
+
+                    <input id={styles.search} className={styles.barradepesquisa} type="text" placeholder='Search' /> {/*Side bar buttons and search*/}
+
+                    <button className={styles.homedojornal}>
+                        <svg className={styles.svghome} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-newspaper" viewBox="0 0 16 16">
+                            <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z" />
+                            <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z" />
+                        </svg>
+                        <span className={styles.hometxtbutton}>Noticias</span></button>
+
+                    <button className={styles.Profilebutton}>
+                        <svg className={styles.profileiconbutton} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" /><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" /></svg>
+                        <span className={styles.profilebuttontxt}>Perfil</span>
+                    </button>
+
+                    <hr className={styles.linhaconfig} />
+
+                    <button className={styles.configbutton}>
+                        <svg className={styles.configicon} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                            <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0" />
+                            <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z" /></svg>
+                        <span className={styles.configtxt}>Configuraçoes</span>
+                    </button>
+
+                </div>
+
+                {/*Lower Profile div*/}
+                <div className={styles.profilediv}>
+                    <svg className={styles.profileicon} xmlns="http://www.w3.org/2000/svg" width="90%" height="90%" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                    </svg>
+                    <p className={styles.profilename}>Name</p>
+                    <p className={styles.emailsidebar}>email.example@gmail.com</p>
+                </div>
+                {/*Button that close or open the side bar*/}
+                <button className={styles.onclosebutton}>
+                    <svg className={styles.svgarrow} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-square-fill" viewBox="0 0 16 16">
+                        <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5.5 10a.5.5 0 0 0 .832.374l4.5-4a.5.5 0 0 0 0-.748l-4.5-4A.5.5 0 0 0 5.5 4z" />
+                    </svg>
+                </button>
+
+            </section>
+            {/*The main of news page*/}
+            <section className={styles.Backgroundmain}>
+                <div className={styles.topdiv}>
+                    <h2 className={styles.Minititle}>Donaduzzi News</h2>
+                    <img src={Logodojornal} className={styles.logodojornal} />
+                </div>
+                <section className={styles.tagsbar}>
+                    <div className={styles.olimpiadas}><p>Olimpiadas</p></div>
+                    <div className={styles.esportes}><p>Esportes</p></div>
+                    <div className={styles.tecnologias}><p>Tecnologias</p></div>
+                    <div className={styles.descobertas}><p>Descobertas</p></div>
+                    <div className={styles.outros}><p>Outros</p></div>
+                </section>
+                <section className={styles.newsspace}>
+                </section>
+            </section>
+        </main>
     )
 }
 
