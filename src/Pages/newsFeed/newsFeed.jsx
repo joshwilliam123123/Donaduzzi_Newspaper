f8fa653b6601686eabe8f449efba295df35dbe2e
Murilo-Dev-Front <murilo.lang.alves@gmail.com>
2026-06-19 11:31:36 -0300

    feed sinc 2.0.0.0.01

--- src/Pages/newsFeed/newsFeed.jsx
+++ src/Pages/newsFeed/newsFeed.jsx
@@ -5,13 +5,12 @@ import Colegio from '../../Imagens/ColegioImagemsuperior.jpg'
     
 const NewsFeed = () => {
     return (
-        <main className="w-full h-full bg-[#e6e6e6]">
-            <HeaderComp></HeaderComp>
+        <main className="w-full h-full">
             <section className=' h-full w-full flex'>
                 <div className='flex flex-row w-full h-full mt-50 bg-white'>
                     <section className='w-full h-full'></section>
                     <section className='h-full w-75 flex flex-col border-l bg-amber-200 border-l-gray-500'></section>
-                    
+
                 </div>
 
             </section>
